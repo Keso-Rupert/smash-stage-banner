4ddde4296fb457d1f71a25f77645b4741f8ee8f3
Keso Rupert <kesorupert@hotmail.com>
2022-12-18 20:51:08 +0100

    Added the stages

---
+++ src/components/Stage.vue
@@ -0,0 +1,21 @@
+<template>
+    <h3>{{ stageTitle }}</h3>
+    <img :src="stageImageSrc" class="stageImage"/>
+</template>
+
+<script>
+    export default {
+        name: 'Stage',
+        props: {
+            stageTitle: String,
+            stageImageSrc: String
+        }
+    }
+</script>
+
+<style>
+    .stageImage{
+        max-width: 500px;
+    }
+
+</style>
